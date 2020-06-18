fc4ea2d715f6f9cfbe87d60ae4fd3cdf1274d64b
amalakho <Anton.Malakhov@intel.com>
2020-06-18 20:40:10 +0300

    one problem at a time

--- Embedded/test/test_readcsv.py
+++ Embedded/test/test_readcsv.py
@@ -6,13 +6,68 @@ import dbe
 
 d = dbe.PyDbEngine('data', 9091)
 assert not d.closed
-root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-table = csv.read_csv(root+"/Tests/Import/datafiles/santander_top1000.csv")
-assert table
+#root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#table = csv.read_csv(root+"/Tests/Import/datafiles/santander_top1000.csv")
+#assert table
 #print(table.to_pandas().head())
-d.consumeArrowTable("santander", table)
+#d.consumeArrowTable("santander", table)
 assert not d.closed
 #assert not table
-print("last step")
-r = d.executeDML("select target from santander")
+print("DDL")
+r = d.executeDDL("""
+CREATE TEMPORARY TABLE trips (
+trip_id BIGINT,
+vendor_id TEXT ENCODING NONE,
+pickup_datetime TIMESTAMP,
+dropoff_datetime TIMESTAMP,
+store_and_fwd_flag TEXT ENCODING DICT,
+rate_code_id BIGINT,
+pickup_longitude DOUBLE,
+pickup_latitude DOUBLE,
+dropoff_longitude DOUBLE,
+dropoff_latitude DOUBLE,
+passenger_count BIGINT,
+trip_distance DOUBLE,
+fare_amount DOUBLE,
+extra DOUBLE,
+mta_tax DOUBLE,
+tip_amount DOUBLE,
+tolls_amount DOUBLE,
+ehail_fee DOUBLE,
+improvement_surcharge DOUBLE,
+total_amount DOUBLE,
+payment_type TEXT ENCODING DICT,
+trip_type BIGINT,
+pickup TEXT ENCODING DICT,
+dropoff TEXT ENCODING NONE,
+cab_type TEXT ENCODING DICT,
+precipitation DOUBLE,
+snow_depth BIGINT,
+snowfall DOUBLE,
+max_temperature BIGINT,
+min_temperature BIGINT,
+average_wind_speed DOUBLE,
+pickup_nyct2010_gid BIGINT,
+pickup_ctlabel DOUBLE,
+pickup_borocode BIGINT,
+pickup_boroname TEXT ENCODING NONE,
+pickup_ct2010 BIGINT,
+pickup_boroct2010 BIGINT,
+pickup_cdeligibil TEXT ENCODING DICT,
+pickup_ntacode TEXT ENCODING DICT,
+pickup_ntaname TEXT ENCODING DICT,
+pickup_puma BIGINT,
+dropoff_nyct2010_gid BIGINT,
+dropoff_ctlabel DOUBLE,
+dropoff_borocode BIGINT,
+dropoff_boroname TEXT ENCODING NONE,
+dropoff_ct2010 BIGINT,
+dropoff_boroct2010 BIGINT,
+dropoff_cdeligibil TEXT ENCODING NONE,
+dropoff_ntacode TEXT ENCODING NONE,
+dropoff_ntaname TEXT ENCODING NONE,
+dropoff_puma BIGINT) WITH (storage_type='CSV:/localdisk1/amalakho/omniscidb/Tests/Import/datafiles/trips_with_headers_top1000.csv', fragment_size=100);""")
+print("last one")
+r = d.executeDML("select count(*) from trips")
+print("done")
 assert r
