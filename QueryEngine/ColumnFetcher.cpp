6511e068a65d93a3a22921b759d4df0de3c19eec
ienkovich <ilya.enkovich@intel.com>
2020-07-24 09:06:26 -0500

    don't compute unnecessary metainfo

--- QueryEngine/ColumnFetcher.cpp
+++ QueryEngine/ColumnFetcher.cpp
@@ -38,14 +38,14 @@ std::pair<const int8_t*, size_t> ColumnFetcher::getOneColumnFragment(
   if (fragment.isEmptyPhysicalFragment()) {
     return {nullptr, 0};
   }
-  auto chunk_meta_it = fragment.getChunkMetadataMap().find(hash_col.get_column_id());
-  CHECK(chunk_meta_it != fragment.getChunkMetadataMap().end());
   const auto& catalog = *executor->getCatalog();
   const auto cd = get_column_descriptor_maybe(
       hash_col.get_column_id(), hash_col.get_table_id(), catalog);
   CHECK(!cd || !(cd->isVirtualCol));
   const int8_t* col_buff = nullptr;
   if (cd) {  // real table
+    auto chunk_meta_it = fragment.getChunkMetadataMap().find(hash_col.get_column_id());
+    CHECK(chunk_meta_it != fragment.getChunkMetadataMap().end());
     ChunkKey chunk_key{catalog.getCurrentDB().dbId,
                        fragment.physicalTableId,
                        hash_col.get_column_id(),
